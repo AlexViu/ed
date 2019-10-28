b78997984d3791eb87c9a07db6fd5dfc0ef645ec
AlexViu <viu.alex771@gmail.com>
2019-10-28 13:39:32 +0100

    ejercicio de un vector y while

--- JHolaMundo/src/iesserpis/ed/Vector.java
+++ JHolaMundo/src/iesserpis/ed/Vector.java
@@ -5,13 +5,25 @@ public class Vector {
 	public static void main(String[] args) {
 		
 		int[] v = new int[] {17, 12, 15, 9, 14};
-		int i ;
-		
-		for (i=0; i <=4; i++) {
-		System.out.println(v[i]);
-		}
+		int x = 9;
+		int index = indexOf(v , x);
+		System.out.println("index=" + index);
 		System.out.println("fin");
-
+		
+		//for (i=0; i <=4; i++) {
+		//System.out.println(v[i]);
+		//}
+		//System.out.println("fin");
+		
 	}
 
+	private static int indexOf(int[] v, int x) {
+		int index = 0;
+		while (index < 5 && v[index]!=x) {
+			index ++;
+		}
+		if (index == 5)
+			return -1;
+			return index;
+	}
 }
