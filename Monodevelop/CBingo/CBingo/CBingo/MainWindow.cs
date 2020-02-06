1d66ba20136608fbdbb24d787bd658f6b8f2cdc0
AlexViu <viu.alex771@gmail.com>
2020-02-06 08:54:10 +0100

    Ejercicio de juego bingo

---
+++ Monodevelop/CBingo/CBingo/CBingo/MainWindow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CBingo;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        Bombo bombo = new Bombo();
+        Panel panel = new Panel(vbox);
+        BJugar.Clicked += delegate {
+
+            int numero = bombo.sacarBola();
+            panel.Marcar(numero);
+        };
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    /*protected void OnBJugarClicked(object sender, EventArgs e)
+    {
+
+        int numero = bombo.sacarBola();
+
+
+    }*/
+
+}
